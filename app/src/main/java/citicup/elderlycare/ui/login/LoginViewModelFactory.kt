8a1058a5a2c1f617ce698be47ddb338b7c4f90c9
Undefined01 <amoscr@163.com>
2022-03-08 13:38:17 +0800

    Initial commit

---
+++ app/src/main/java/citicup/elderlycare/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package citicup.elderlycare.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import citicup.elderlycare.data.LoginDataSource
+import citicup.elderlycare.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
---
+++ app/src/main/java/citicup/elderlycare/ui/login/StartActivity.kt
@@ -0,0 +1,10 @@
+package citicup.elderlycare.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
